f179d35fcd735a2ca48b1dfab3abe0f993f422a5
yuncrop <sun.mengmeng@yuncrop.com>
2015-06-23 16:26:55 +0800

    Update NetUrls.java

--- src/com/xinnongyun/config/NetUrls.java
+++ src/com/xinnongyun/config/NetUrls.java
@@ -7,67 +7,67 @@ package com.xinnongyun.config;
  */
 public class NetUrls {
 
-	public static final String AppUseHelp = "http://yuncrop.com/";
+	public static final String AppUseHelp = "https://yuncrop.com/";
 
 
 	//获取农场中控列表
 	public static final String getFarmAllControlsInfo(String farmId)
 	{
-		return "http://m.nnong.com/v2/api-automator/cc/?farm=" + farmId;
+		return "https://m.nnong.com/v2/api-automator/cc/?farm=" + farmId;
 	}
 	
 	//获取中控信息 serial
 	public static final String getControlInfoBySerial(String serial)
 	{
-		return "http://m.nnong.com/v2/api-automator/cc/?serial=" + serial;
+		return "https://m.nnong.com/v2/api-automator/cc/?serial=" + serial;
 	}
 		
 	//获取中控信息 id
 	public static final String getControlInfoById(String id)
 	{
-		return "http://m.nnong.com/v2/api-automator/cc/" + id + "/";
+		return "https://m.nnong.com/v2/api-automator/cc/" + id + "/";
 	}
 	
 	//修改中控信息 serial
 	public static final String modifyControlInfo(String id)
 	{
-		return "http://m.nnong.com/v2/api-automator/cc/" + id + "/";
+		return "https://m.nnong.com/v2/api-automator/cc/" + id + "/";
 	}
 	
 	//安装中控
 	public static final String mountControl(String id)
 	{
-		return "http://m.nnong.com/v2/api-automator/cc/" + id + "/mount/";
+		return "https://m.nnong.com/v2/api-automator/cc/" + id + "/mount/";
 	}
 	
 	//卸载中控
 	public static final String unmountControl(String id)
 	{
-		return "http://m.nnong.com/v2/api-automator/cc/" + id + "/unmount/";
+		return "https://m.nnong.com/v2/api-automator/cc/" + id + "/unmount/";
 	}
 
 	//获取中控下所有控制单元
 	public static final String getAllBlowerFromControl(String controlId)
 	{
-		return "http://m.nnong.com/v2/api-automator/blower/?cc=" + controlId;
+		return "https://m.nnong.com/v2/api-automator/blower/?cc=" + controlId;
 	}
 	
 	//获取控制单元信息
 	public static final String getBlowerInfoById(String id)
 	{
-		return "http://m.nnong.com/v2/api-automator/blower/" + id + "/";
+		return "https://m.nnong.com/v2/api-automator/blower/" + id + "/";
 	}
 	
 	//修改放风机信息
 	public static final String modifyBlowerInfos()
 	{
-		return "http://m.nnong.com/v2/api-automator/blower/";
+		return "https://m.nnong.com/v2/api-automator/blower/";
 	}
 	
 	//移动放风机风口
 	public static final String moveBlowerDistance()
 	{
-		return "http://m.nnong.com/v2/api-automator/blower/move/";
+		return "https://m.nnong.com/v2/api-automator/blower/move/";
 	}
 	
 
@@ -75,20 +75,20 @@ public class NetUrls {
 	//重置放风机
 	public static final String resetBlower()
 	{
-		return "http://m.nnong.com/v2/api-automator/blower/reset/";
+		return "https://m.nnong.com/v2/api-automator/blower/reset/";
 	}
 	
 	//获取控元最新数据
 	public static final String getUnitLastestInfo(String id)
 	{
-		return "http://m.nnong.com/v2/api-automator/blower-env/observe/?blower=" + id;
+		return "https://m.nnong.com/v2/api-automator/blower-env/observe/?blower=" + id;
 	}
 	
 	
 	//获取控元一段时间内的环境信息序列
 	public static final String getUnitOneDayInfo(String id,String fromTime,String endTime)
 	{
-		return "http://m.nnong.com/v2/api-automator/blower-env/?blower=" + id + "&begin_at=" + fromTime + "&end_at=" + endTime;
+		return "https://m.nnong.com/v2/api-automator/blower-env/?blower=" + id + "&begin_at=" + fromTime + "&end_at=" + endTime;
 	}
 	
 	
@@ -97,72 +97,72 @@ public class NetUrls {
 	/**
 	 * 萤云石地址
 	 */
-	public static final String URIYINGYUNSHI = "https://open.ys7.com/api/method";
+	public static final String URIYINGYUNSHI = "httpss://open.ys7.com/api/method";
 	
 	
-	public static final String URL_CAMERA_ADD = "http://m.nnong.com/v2/api-camera/";
+	public static final String URL_CAMERA_ADD = "https://m.nnong.com/v2/api-camera/";
 	
 	
 	public static final String URI_CAMREA_GET_BY__ID(String id)
 	{
-		return " http://m.nnong.com/v2/api-camera/camera/" + id + "/";
+		return " https://m.nnong.com/v2/api-camera/camera/" + id + "/";
 	}
 	
 	public static final String URI_CAMREA_GET_BY__SERIAL(String serial)
 	{
-		return "http://m.nnong.com/v2/api-camera/camera/?device_serial=" + serial;
+		return "https://m.nnong.com/v2/api-camera/camera/?device_serial=" + serial;
 	}
 	
 	public static final String URI_CAMREAList(String farm)
 	{
-		return "http://m.nnong.com/v2/api-camera/farm/?farm=" + farm;
+		return "https://m.nnong.com/v2/api-camera/farm/?farm=" + farm;
 	}
 	
 	
 	public static final String URL_CAMREA_MODIFY_ADD(String id)
 	{
-		return "http://m.nnong.com/v2/api-camera/" + id + "/";
+		return "https://m.nnong.com/v2/api-camera/" + id + "/";
 	}
 	
 	public static final String URL_CAMREA_DELETE(String id)
 	{
-		return "http://m.nnong.com/v2/api-camera/" + id + "/unmount/";
+		return "https://m.nnong.com/v2/api-camera/" + id + "/unmount/";
 	}
 	
 	
 	//修改环境小站
 	public static final String URL_WEATHER_STATIONSINFOCHANGE(String weatherId)
 	{
-		return "http://m.nnong.com/v2/api-weather-station/"+ weatherId +"/";
+		return "https://m.nnong.com/v2/api-weather-station/"+ weatherId +"/";
 	}
 		 
 	//获取农场下所有的环境小站实时数据
 	public static final String URL_WEATHER_STATIONSTimeLine(String weatherId)
 	{
-		return "http://m.nnong.com/v2/api-weather-station/" + weatherId +"/observe/";
+		return "https://m.nnong.com/v2/api-weather-station/" + weatherId +"/observe/";
 	}
 	
 	//获取农场下所有的环境小站实时数据
 	public static final String URL_WEATHER_STATIONSAVG30(String weatherId)
 	{
-		return "http://m.nnong.com/v2/api-weather-station/" + weatherId + "/avg30/";
+		return "https://m.nnong.com/v2/api-weather-station/" + weatherId + "/avg30/";
 	}
 	
 	
 	//获取农场下所有的环境小站
 	public static final String URL_WEATHER_STATIONS(String farmId)
 	{
-		return "http://m.nnong.com/v2/api-weather-station/farm/?farm=" + farmId;
+		return "https://m.nnong.com/v2/api-weather-station/farm/?farm=" + farmId;
 	}
 	
 	
 	//七牛token路径
-	public static final String URL_QINIU_TOKEN = "http://www.nnong.com/api-journal/token/";
+	public static final String URL_QINIU_TOKEN = "https://www.nnong.com/api-journal/token/";
 	
 	
 	public static String get_URL_JOURNALVIDEO_BYID(String journal_id)
 	{
-		return "http://m.nnong.com/api-journal/" + journal_id + "/";
+		return "https://m.nnong.com/api-journal/" + journal_id + "/";
 	}
 	
 	
@@ -173,7 +173,7 @@ public class NetUrls {
 	 */
 	public static  String getURL_JOURNALVIDEO_LIKE(String journal_id)
 	{
-		return "http://m.nnong.com/api-journal/" + journal_id + "/like/";
+		return "https://m.nnong.com/api-journal/" + journal_id + "/like/";
 	}
 	
 	/**
@@ -183,7 +183,7 @@ public class NetUrls {
 	 */
 	public static  String getURL_JOURNALVIDEO_VIEWS(String journal_id)
 	{
-		return "http://m.nnong.com/api-journal/" + journal_id + "/view/";
+		return "https://m.nnong.com/api-journal/" + journal_id + "/view/";
 	}
 	
 	/**
@@ -193,100 +193,100 @@ public class NetUrls {
 	 */
 	public static String getURL_JOURNALVIDEO_COMMENTS(String journal_id)
 	{
-		 return "http://m.nnong.com/api-journal/" + journal_id + "/comment/";
+		 return "https://m.nnong.com/api-journal/" + journal_id + "/comment/";
 	}
 	
 	//视频日志api
-	public static final String URL_VIDEO_CREATE = "http://www.nnong.com/api-journal/";
+	public static final String URL_VIDEO_CREATE = "https://www.nnong.com/api-journal/";
 	public static final String URL_VIDEO_DELETE(String id)
 	{
-		return "http://www.nnong.com/api-journal/" + id + "/";
+		return "https://www.nnong.com/api-journal/" + id + "/";
 	}
 	
 	
-	public static final String URL_NOTICE_GETALL = "http://m.nnong.com/api-notice/";
+	public static final String URL_NOTICE_GETALL = "https://m.nnong.com/api-notice/";
 	public static String getUpdateNotice(String id)
 	{
-		return "http://m.nnong.com/api-notice/"+ id +"/";
+		return "https://m.nnong.com/api-notice/"+ id +"/";
 	}
 	
 	//新建种养记录
-	public static final String URL_FARMING_NEW = "http://m.nnong.com/api-farming/";
+	public static final String URL_FARMING_NEW = "https://m.nnong.com/api-farming/";
 	public static final String URL_FARMING_GETALL(String farmId)
 	{
-		return "http://m.nnong.com/api-farming/?farm=" + farmId;
+		return "https://m.nnong.com/api-farming/?farm=" + farmId;
 	}
 	
 	
 	public static final String URL_FARMING_MODIFY(String farmId)
 	{
-		return "http://m.nnong.com/api-farming/" + farmId+ "/";
+		return "https://m.nnong.com/api-farming/" + farmId+ "/";
 	}
 	
 	/**
 	 * 意见反馈 POST 'content=test'
 	 */
-	public static final String URL_SUGGESTION = "http://m.nnong.com/api-feedback/";
+	public static final String URL_SUGGESTION = "https://m.nnong.com/api-feedback/";
 	
 	/**
 	 * 找回密码
 	 */
-	public static final String FINDPWD = "http://m.nnong.com/api-user/reset_password/";
+	public static final String FINDPWD = "https://m.nnong.com/api-user/reset_password/";
 	
 	
 	/**
 	 * 二维码序列判断
 	 */
-	public static final String URL = "http://m.nnong.com";
-	public static final String URL2 = "http://m.nnonq.com";
+	public static final String URL = "https://m.nnong.com";
+	public static final String URL2 = "https://m.nnonq.com";
 	
 	
 	//创建、获取农场
 	// /api-farm/ -H 'Authorization: Token b23d1ff85398571932801bde0ae3f73e3e5e595c' -d 'name=测试农场1&bg=&description=&lat=&lng=&location=&logo='
-	public static final String URL_FARM_CREATE = "http://m.nnong.com/api-farm/";
-	public static final String URL_FARMMANAGER = "http://m.nnong.com/farm/";
+	public static final String URL_FARM_CREATE = "https://m.nnong.com/api-farm/";
+	public static final String URL_FARMMANAGER = "https://m.nnong.com/farm/";
 	//获取所属农村的信息
-	public static final String URL_FARMMANAGER_GETMINE = "http://m.nnong.com/api-farm/mine/";
+	public static final String URL_FARMMANAGER_GETMINE = "https://m.nnong.com/api-farm/mine/";
 	//修改农场
 	public static String URL_MODIFY_FARM(String farmId)
 	{
-		return "http://m.nnong.com/api-farm/" + farmId + "/";
+		return "https://m.nnong.com/api-farm/" + farmId + "/";
 	}
 	
 	//退出农场
 	public static String URL_EXIT_FARM(String farmId)
 	{
-		return "http://m.nnong.com/api-farm/"+ farmId +"/depart/";
+		return "https://m.nnong.com/api-farm/"+ farmId +"/depart/";
 	}
 	
 	//删除农场
 	public static String URL_DELETE_FARM(String farmId)
 	{
-		return "http://m.nnong.com/api-farm/" + farmId + "/";
+		return "https://m.nnong.com/api-farm/" + farmId + "/";
 	}
 	
 	//更新采集器信息
 	public static String URL_COLLECT_UPDATE(String collectID)
 	{
-		return "http://m.nnong.com/api-collector/" + collectID + "/";
+		return "https://m.nnong.com/api-collector/" + collectID + "/";
 	}
 	
 	//获取当前农场的种养盒子
-	public static String URL_FARMING_COLLECTS = "http://m.nnong.com/api-farming/";
+	public static String URL_FARMING_COLLECTS = "https://m.nnong.com/api-farming/";
 	
 	//获取农场下所有盒子信息
-	public static String URL_FARM_COLLECTS = "http://m.nnong.com/api-collector/";
+	public static String URL_FARM_COLLECTS = "https://m.nnong.com/api-collector/";
 	
 	//获取盒子信息
 	public static String URL_COLLECTOR_GET(String collectserial)
 	{
-		return "http://m.nnong.com/api-collector/?serial=" + collectserial + "";
+		return "https://m.nnong.com/api-collector/?serial=" + collectserial + "";
 	}
 	
 	//获取盒子信息
 	public static String URL_GATEWAY_GET(String id)
 	{
-		return "http://m.nnong.com/api-gateway/" + id + "/";
+		return "https://m.nnong.com/api-gateway/" + id + "/";
 	}
 		
 	
@@ -302,15 +302,15 @@ public class NetUrls {
 	 * --------
 	 * 200(ok); 400(参数错误); 401(未授权); 403(没有权限); 404(该动物/作物不存在)
 	 */
-	public static final String URL_BEINGS = "http://m.nnong.com/api-being/reserved/";
-	public static final String URL_BEINGS_FARM = "http://m.nnong.com/api-being/";
+	public static final String URL_BEINGS = "https://m.nnong.com/api-being/reserved/";
+	public static final String URL_BEINGS_FARM = "https://m.nnong.com/api-being/";
 	/**
 	 * 土壤管理
 	 * 获取土壤类型 GET user_serial, password
 	 * -----
 	 * 200(ok); 400(参数错误); 401(未授权)
 	 */
-    public static final String URL_SOIL = "http://m.nnong.com/api-soil/reserved/";
+    public static final String URL_SOIL = "https://m.nnong.com/api-soil/reserved/";
 	
     
     /**
@@ -319,7 +319,7 @@ public class NetUrls {
      * --------
      * 200(ok); 400(参数错误); 401(未授权); 404(该盒子不存在)
      */
-    public static final String URL_TIMELINE = "http://m.nnong.com/api-timeline/";
+    public static final String URL_TIMELINE = "https://m.nnong.com/api-timeline/";
     
     
     /**
@@ -329,7 +329,7 @@ public class NetUrls {
      * -------
      * 200(ok); 400(参数错误); 401(未授权); 404(该盒子不存在)
      */
-    public static final String URL_K_TIMELINE = "http://m.nnong.com/api-kline/";
+    public static final String URL_K_TIMELINE = "https://m.nnong.com/api-kline/";
     
     
     /**
@@ -338,29 +338,29 @@ public class NetUrls {
      * -----
      * 200(ok)
      */
-    public static final String URL_VERSION = "http://m.nnong.com/api-version/latest/";
+    public static final String URL_VERSION = "https://m.nnong.com/api-version/latest/";
     public static final String URL_VERSION_LAST = "version_code";
     public static final String URL_VERSION_URL = "package";
     
 	/**
 	 * 账户信息管理路径 登录get   注册post
 	 */
-    public static final String URL_LOGIN = "http://m.nnong.com/api-user/login/";
-    public static final String URL_LOGIN_ZHUCE = "http://m.nnong.com/api-user/register/";
-    public static final String URL_MODIFY_USERINFO = "http://m.nnong.com/api-user/";
+    public static final String URL_LOGIN = "https://m.nnong.com/api-user/login/";
+    public static final String URL_LOGIN_ZHUCE = "https://m.nnong.com/api-user/register/";
+    public static final String URL_MODIFY_USERINFO = "https://m.nnong.com/api-user/";
     public static String URL_MOFDIFYUSERPWD(String id)
     {
-    	return "http://m.nnong.com/api-user/set_password/";          //"http://m.nnong.com/user/" + id + "/set_password/";
+    	return "https://m.nnong.com/api-user/set_password/";          //"https://m.nnong.com/user/" + id + "/set_password/";
     }
     
-	//public static final String URL_LOGIN_ZHUCE = "http://m.nnong.com/account/api";
+	//public static final String URL_LOGIN_ZHUCE = "https://m.nnong.com/account/api";
 	public static final String URL_LOGIN_ZHUCE_USERNAME = "username";
 	public static final String URL_LOGIN_ZHUCE_PASSWORD = "password";
 	
 	/**
 	 * 获取验证码
 	 */
-	public static final String URL_ZHUCE_GETYAZHENGMA="http://m.nnong.com/api-sms/verify/";
+	public static final String URL_ZHUCE_GETYAZHENGMA="https://m.nnong.com/api-sms/verify/";
 	public static final String URL_ZHUCE_GETYAZHENGMA_PHONE = "phone_number";
 	public static final String URL_ZHUCE_USERNAME = "username";
 	//验证码的返回关键字
